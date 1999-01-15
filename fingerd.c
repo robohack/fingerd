0ea61ce800a5b36f2789c9dd7db393e92ce439a4
Greg A. Woods <woods@planix.ca>
1999-01-15 20:00:00 +0000

    - change '-f' to mean "allow forwarding". - change '-p finger' to '-P finger' - add support for NetBSD's finger options (-p, -g, -h, etc.). - read the client's command before doing the DNS so it can be logged. - show the motd before doing any options handling. - make sure all strdup()s are checked for errors. - use tcpd.h from system if HAVE_LIBRAP. - new copyright from COPYING. - fix #ident.

--- fingerd.c
+++ fingerd.c
@@ -1,36 +1,35 @@
 /*
  * Copyright 1994 Michael Shanzer.  All rights reserved.
+ * Copyright 1999 Greg A. Woods <woods@planix.com.  All rights reserved.
  *
- * Permission is granted to anyone to use this software for any purpose on
- * any computer system, and to alter it and redistribute it freely, subject
- * to the following restrictions:
+ * Permission is granted to anyone to use this software for any purpose on any
+ * computer system, and to alter it and redistribute it freely, subject to the
+ * following restrictions:
  *
- * Use of this software constitutes acceptance for use in an AS IS
- * condition. There are NO warranties with regard to this software.
- * In no event shall the author be liable for any damages whatsoever
- * arising out of or in connection with the use or performance of this
- * software.  Any use of this software is at the user's own risk.
+ * Use of this software constitutes acceptance for use in an AS IS condition.
+ * There are NO warranties with regard to this software.  In no event shall the
+ * authors be held liable for any damages whatsoever arising out of, or in
+ * connection with, the use or performance of this software.  Any use of this
+ * software is strictly at the user's own risk.
  *
- * The origin of this software must not be misrepresented, either by
- * explicit claim or by omission.  Since few users ever read sources,
- * credits must appear in the documentation.
+ * The origin of this software must not be misrepresented, either by explicit
+ * claim or by omission.  Since few users ever read sources, credits must also
+ * appear in the documentation.
  *
- * If you make modifications to this software that you feel
- * increases it usefulness for the rest of the community, please
- * email the changes, enhancements, bug fixes as well as any and
- * all ideas to me. This software is going to be maintained and
- * enhanced as deemed necessary by the community.
+ * If you make modifications to this software that you feel increases it
+ * usefulness for the rest of the community, please email the changes,
+ * enhancements, bug fixes as well as any and all ideas to the authors.  This
+ * software is going to be maintained and enhanced as deemed necessary by the
+ * community.
  *
  *              Michael S. Shanzer
  *  		shanzer@foobar.com
  *
+ *              Greg A. Woods
+ *  		woods@planix.com
  */
 
-/*
- * fingerd main loop
- */
- 
-#ident	"@(#)fingerd:$Name:  $:$Id: fingerd.c,v 1.7 1999/01/15 00:22:10 woods Exp $"
+#ident	"@(#)$Name:  $:$Id: fingerd.c,v 1.8 1999/01/15 20:00:00 woods Exp $"
 
 #ifdef HAVE_CONFIG_H
 # include "config.h"
@@ -75,9 +74,11 @@ extern int errno;
 #include <stdio.h>
 
 #include "fingerd.h"
-#include "tcpd.h"			/* XXX the local one, not any system one! */
-
-char unknown[] = NO_IDENT_REPLY;
+#ifdef HAVE_LIBWRAP
+# include <tcpd.h>
+#else
+# include "tcpd.h"			/* XXX the local one, not any system one! */
+#endif
 
 extern	char	*optarg;
 extern	int	optind, opterr;
@@ -87,52 +88,72 @@ main(argc, argv)
 	int             argc;
 	char           *argv[];
 {
-	struct hostent		*hp;
-	struct sockaddr_in	sin, laddr;
+	struct hostent *hp;
+	struct sockaddr_in sin;
+	struct sockaddr_in laddr;
 	unsigned long	perm = ACCESS_GRANTED;
-	FILE		*fp;
-	char		*lp;
-	int		logging = FALSE,
-			nolist = FALSE,
-			noforward = FALSE,
-			doident = FALSE,
-			forceident = FALSE,
-			forceresolv = FALSE,
-			forceshort = FALSE,
-			defaultshort = FALSE,
-			nomatch = FALSE,
-			ac = 1,
-			ch,
-			sval;
-	char		*ap,
-			*prog,
-			*rhost,
-			*ruser = NULL,
-			line[BUFSIZ],
-			*av[BUFSIZ];
+	FILE           *fp;
+	char           *lp;
+	int             logging = FALSE;
+	int             forward = FALSE;
+	int             nogecos = FALSE;
+	int             nohome = FALSE;
+	int             nolist = FALSE;
+	int             noplan = FALSE;
+	int             showhost = FALSE;
+	int             doident = FALSE;
+	int             forceident = FALSE;
+	int             forceresolv = FALSE;
+	int             forceshort = FALSE;
+	int             defaultshort = FALSE;
+	int             nomatch = FALSE;
+	int             ac = 1;
+	int             ch;
+	int             sval;
+	char           *ap;
+	char           *prog;
+	char           *rhost;
+	char           *ruser = NULL;
+	char            line[BUFSIZ];
+	char           *av[BUFSIZ];
 
 	prog = FINGER_PATH;
-	openlog("fingerd", LOG_PID, FINGER_SYSLOG);
+	openlog("fingerd", LOG_PID, FINGERD_SYSLOG);
 	opterr = 0;
-	while ((ch = getopt(argc, argv, "lp:ufiIsSmbrV")) != EOF) {
+	while ((ch = getopt(argc, argv, "bfghiIlmpP:rsSuV")) != EOF) {
 		switch (ch) {
-		case 'l':			
-			logging = TRUE;
+		case 'b':
+			nohome = TRUE;
 			break;
-		case 'p':			
-			prog = optarg;
+		case 'f':
+			forward = TRUE;
 			break;
-		case 'u':			
-			nolist = TRUE;
+		case 'g':
+			nogecos = TRUE;
 			break;
-		case 'f':			
-			noforward = TRUE;
-			break;		
+		case 'h':
+			showhost = TRUE;
+			break;
 		case 'i':
 			doident = TRUE;
 			break;
-		case 'I':			
+		case 'I':
 			forceident = TRUE;
+			break;
+		case 'l':
+			logging = TRUE;
+			break;
+		case 'm':
+			nomatch = TRUE;
+			break;
+		case 'p':
+			noplan = TRUE;
+			break;
+		case 'P':
+			prog = optarg;
+			break;
+		case 'r':
+			forceresolv = TRUE;
 			break;
 		case 's':
 			defaultshort = TRUE;
@@ -140,14 +161,8 @@ main(argc, argv)
 		case 'S':
 			forceshort = TRUE;
 			break;
-		case 'm':
-			nomatch = TRUE;
-			break;
-		case 'b':
-			av[ac++] = strdup("-b");
-			break;
-		case 'r':
-			forceresolv = TRUE;
+		case 'u':
+			nolist = TRUE;
 			break;
 		case 'V':
 			puts(version);
@@ -162,17 +177,23 @@ main(argc, argv)
 		err("getpeername: %s", strerror(errno));
 	if (getsockname(0, (struct sockaddr *) &laddr, &sval) < 0)
 		err("getsockname: %s", strerror(errno));
-		
+
 	if (doident || forceident) {
-		char idreply[STRING_LENGTH];
+		char idreply[STRING_LENGTH + 1];
 
 		rfc931(&sin, &laddr, idreply);
 		if (!(ruser = strdup(idreply)))
 			err("strdup: no memory - %s", strerror(errno));
-	} else { 
+	} else {
 		if (!(ruser = strdup(NO_IDENT_DONE)))
 			err("strdup: no memory - %s", strerror(errno));
 	}
+	if (!fgets(line, sizeof(line), stdin)) {
+		exit(1);
+		/* NOTREACHED */
+	}
+	ap = strtok(line, "\r\n");	/* trim EOL */
+
 	if ((hp = gethostbyaddr((char *) &sin.sin_addr.s_addr,
 				sizeof(sin.sin_addr.s_addr), AF_INET))) {
 		/*
@@ -184,8 +205,8 @@ main(argc, argv)
 		if (!(hp = gethostbyname(rhost))) {
 			if (forceresolv) {
 				syslog(LOG_NOTICE,
-				       "from=%s@[%s] to=[unknown] stat=Cannot re-resolve %s",
-				       ruser, inet_ntoa(sin.sin_addr), rhost);
+				       "from=%s@[%s] to='%s'; Cannot re-resolve %s",
+				       ruser, ap, inet_ntoa(sin.sin_addr), rhost);
 				printf("Possible name server mis-configuration.\nSorry, I cannot resolve your hostname '%s' [%s].",
 				       rhost, inet_ntoa(sin.sin_addr));
 				exit(1);
@@ -205,9 +226,9 @@ main(argc, argv)
 			}
 			if (!match) {
 				syslog(LOG_NOTICE,
-				       "from=%s@[%s] to=[unknown] stat=host %s does not resolve to [%s].",
-				       ruser, inet_ntoa(sin.sin_addr), rhost, inet_ntoa(sin.sin_addr));
-				printf("Name server mis-configuration.\nSorry, your host '%s' does not have the address [%s].\n",
+				       "from=%s@[%s] to='%s'; host %s does not resolve to [%s].",
+				       ruser, ap, inet_ntoa(sin.sin_addr), rhost, inet_ntoa(sin.sin_addr));
+				printf("Name server mis-configuration.\nYour host '%s' does not have the address [%s].\n",
 				       rhost, inet_ntoa(sin.sin_addr));
 				exit(1);
 				/* NOTREACHED */
@@ -225,66 +246,102 @@ main(argc, argv)
 			/* NOTREACHED */
 		}
 	}
-	if (!fgets(line, sizeof(line), stdin)) {
-		exit(1);
-		/* NOTREACHED */
-	}
-	ap = strtok(line, "\r\n");
-
-	perm = access_check(ruser, rhost); /* only after resolving is done */
+	perm = access_check(ruser, rhost);
 	if (perm & ACCESS_DENIED) {
 		if (logging) {
 			syslog(LOG_NOTICE,
 			       "from=%s@%s to=%s stat=Refused",
 			       ruser, rhost, (ap ? ap : "[user list]"));
 		}
-		puts("Access to finger services denied.");
+		puts("Access to finger service denied.");
 		exit(1);
 		/* NOTREACHED */
 	}
+	/*
+	 * Now we adjust options based on potentially more specific refinements
+	 * in the access file.  Note that the logic is designed in such a way
+	 * as to make it impossible to override command-line options from the
+	 * access file.
+	 */
 	if (perm & ACCESS_NOLIST)
 		nolist = TRUE;
-	if (perm & ACCESS_NOFORWARD)
-		noforward = TRUE;
+	if (perm & ACCESS_NOPLAN)
+		noplan = TRUE;
+	if (perm & ACCESS_FORWARD)
+		forward = TRUE;
+	if (perm & ACCESS_NOGECOS)
+		nogecos = TRUE;
+	if (perm & ACCESS_NOHOME)
+		nohome = TRUE;
 	if (perm & ACCESS_NOMATCH)
 		nomatch = TRUE;
+	if (perm & ACCESS_FORCEIDENT)
+		forceident = TRUE;
+	if (perm & ACCESS_SHOWHOST)
+		showhost = TRUE;
 	if (perm & ACCESS_FORCESHORT)
 		forceshort = TRUE;
 	if (perm & ACCESS_DEFAULTSHORT)
 		defaultshort = TRUE;
-	if (perm & ACCESS_FORCEIDENT)
-		forceident = TRUE;
-	if (forceident && (strcmp(ruser, NO_IDENT_REPLY) == 0 ||
+	if (forceident && (strcmp(ruser, STRING_UNKNOWN) == 0 ||
 			   strcmp(ruser, NO_IDENT_DONE) == 0)) {
 		if (logging) {
 			syslog(LOG_NOTICE,
-			       "from=%s@%s to=%s stat=No ident server",
+			       "from=%s@%s to=%s; no identification available",
 			       ruser, rhost, (ap ? ap : "[user list]"));
 		}
 		if (strcmp(ruser, NO_IDENT_DONE) == 0)
-			puts("Access to finger services denied.  Valid 'ident' not available.");
+			puts("Access to finger services denied.  Valid identification not available.");
 		else
-			puts("Access to finger services denied.  Valid 'ident' reply required.");
-		exit(1);	
+			puts("Access to finger services denied.  Valid identification reply required.");
+		exit(1);
 		/* NOTREACHED */
 	}
-	lp = ap;
-	ap = strtok(lp, " ");
+	if ((fp = fopen(FINGERD_MOTD, "r"))) {
+		char	mline[BUFSIZ];
+
+		while (fgets(mline, sizeof(line), fp))
+			puts(mline);
+		puts("----");
+		fclose(fp);
+	}
+	lp = ap;			/* keep pointer to first token */
+	ap = strtok(lp, " ");		/* split on possible space separator */
 	if (forceshort) {
+		/* ignore any '/W' request for verbose output */
 		if (!(av[ac++] = strdup("-s")))
 			err("strdup: no memory - %s", strerror(errno));
 	} else if (ap && ap[0] == '/' && toupper(ap[1]) == 'W') {
-		av[ac++] = strdup("-l");
-		ap = strtok((char *) NULL, " ");
+		if (!(av[ac++] = strdup("-l")))
+			err("strdup: no memory - %s", strerror(errno));
+		ap = strtok((char *) NULL, " "); /* look for more tokens */
 	} else {
 		if (defaultshort) {
 			if (!(av[ac++] = strdup("-s")))
 				err("strdup: no memory - %s", strerror(errno));
 		}
 	}
-	if (nomatch)
-		av[ac++] = strdup("-m");
-	if (noforward && (ap && strchr(ap, '@'))) {
+	if (nomatch) {
+		if (!(av[ac++] = strdup("-m")))
+			err("strdup: no memory - %s", strerror(errno));
+	}
+	if (nogecos) {
+		if (!(av[ac++] = strdup("-g")))
+			err("strdup: no memory - %s", strerror(errno));
+	}
+	if (nohome) {
+		if (!(av[ac++] = strdup("-b")))	/* not in all finger(1)s */
+			err("strdup: no memory - %s", strerror(errno));
+	}
+	if (noplan) {
+		if (!(av[ac++] = strdup("-p")))
+			err("strdup: no memory - %s", strerror(errno));
+	}
+	if (showhost) {
+		if (!(av[ac++] = strdup("-h")))
+			err("strdup: no memory - %s", strerror(errno));
+	}
+	if (!forward && (ap && strchr(ap, '@'))) {
 		if (logging) {
 			syslog(LOG_NOTICE,
 			       "from=%s@%s to=%s stat=Refused Forwarding",
@@ -308,29 +365,19 @@ main(argc, argv)
 		syslog(LOG_NOTICE, "from=%s@%s to=%s stat=OK",
 		       ruser, rhost, (ap ? ap : "[user list]"));
 	}
-	if ((fp = fopen(FINGER_MOTD, "r"))) {
-		char	mline[BUFSIZ];
-
-		while (fgets(mline, sizeof(line), fp))
-			fputs(mline, stdout);
-		puts("----");
-		fclose(fp);
-	}
 	if (execute_user_cmd(ap, ruser, rhost)) {
 		exit(0);
 		/* NOTREACHED */
 	} else {
-		if (ap)
-			av[ac++] = strdup(ap);
+		if (ap) {
+			if (!(av[ac++] = strdup(ap)))
+				err("strdup: no memory - %s", strerror(errno));
+		}
 	}
-	av[ac] = NULL;
-	if ((lp = strrchr(prog, '/')) != NULL) {
-		if (!(av[0] = strdup(++lp)))
-			err("strdup: no memory - %s", strerror(errno));
-	} else {
-		if (!(av[0] = strdup(prog)))
-			err("strdup: no memory - %s", strerror(errno));
-	}
+	av[ac] = NULL;			/* no more arguments!  ;-) */
+	lp = (lp = strrchr(prog, '/')) ? (lp + 1) : prog;
+	if (!(av[0] = strdup(lp)))
+		err("strdup: no memory - %s", strerror(errno));
 	if (!execute(prog, av))
 		err("execute: %s", strerror(errno));
 
