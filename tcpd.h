8aeddd8032a7f72c6e0b5fcff257117f8ff09616
Greg A. Woods <woods@planix.ca>
1999-01-15 18:07:22 +0000

    - define STRING_UNKNOWN so we're API-compatible with the real one

--- tcpd.h
+++ tcpd.h
@@ -1,11 +1,12 @@
- /*
-  * hacked version that's useful with just rfc931.c
-  */
+/*
+ * a much hacked version that's useful with just rfc931.c
+ */
 
-#ident	"@(#)fingerd:$Name:  $:$Id: tcpd.h,v 1.1 1997/09/15 00:40:34 woods Exp $"
+#ident	"@(#)fingerd:$Name:  $:$Id: tcpd.h,v 1.2 1999/01/15 18:07:22 woods Exp $"
 
 #define RFC931_TIMEOUT	10		/* seconds */
 #define STRING_LENGTH	128		/* as per tcp_wrappers 7.6 */
+#define STRING_UNKNOWN	"[Unknown]"	/* lookup failed (tcp_wrappers uses "unknown") */
 
 #define STRN_CPY(d,s,l) { strncpy((d),(s),(l)); (d)[(l)-1] = 0; }
 
