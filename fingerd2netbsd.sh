69e08ec009b2e2e3569bf01dfaabf7390791ab1e
Greg A. Woods <woods@planix.ca>
1999-01-17 01:19:50 +0000

    - add subst for @PATH_SYSCONFDIR@ (for manual page). - use fingerd.8.in as source for manual page (with new subst). - adjust for renamed samples files. - forcably remove the import.d directory - fix some typos.

--- fingerd2netbsd.sh
+++ fingerd2netbsd.sh
@@ -5,7 +5,7 @@
 #
 # NOTE: needs a POSIX /bin/sh to run....
 #
-#ident	"@(#)$Name:  $:$Id: fingerd2netbsd.sh,v 1.3 1999/01/15 20:36:40 woods Exp $"
+#ident	"@(#)fingerd:$Name:  $:$Id: fingerd2netbsd.sh,v 1.4 1999/01/17 01:19:50 woods Exp $"
 
 PWD=$(/bin/pwd)
 version=$(basename $PWD | sed -e 's/fingerd-//')
@@ -26,21 +26,22 @@ cpsed ()
 ' \
 	    -e '/^\.\\"#ident/i\
 .\\"#ident	"@(#)$\Name$:$\NetBSD$"\
-' $1 > $2
+' \
+	    -e 's;@\PATH_SYSCONFDIR@;/etc;' $1 > $2
 }
 
 IMPORTDIR="import.d"
 
+rm -rf ${IMPORTDIR}
 mkdir ${IMPORTDIR}
 
-if [ ! -r Makefile.BSD -o ! -r fingerd.8 ] ; then
+if [ ! -r Makefile.BSD ] ; then
 	./configure
 fi
 
-# note the rename....
+# note the renames....
 cpsed Makefile.BSD ${IMPORTDIR}/Makefile
-
-cpsed fingerd.8 ${IMPORTDIR}/fingerd.8
+cpsed fingerd.8.in ${IMPORTDIR}/fingerd.8
 cpsed fingerd.c ${IMPORTDIR}/fingerd.c
 cpsed fingerd.h ${IMPORTDIR}/fingerd.h
 cpsed access.c ${IMPORTDIR}/access.c
@@ -51,13 +52,13 @@ cpsed fingerd2netbsd.sh ${IMPORTDIR}/fingerd2netbsd.sh
 
 mkdir ${IMPORTDIR}/samples
 
-cpsed samples/access ${IMPORTDIR}/samples/fingerd.acl
-cpsed samples/motd ${IMPORTDIR}/samples/fingerd.motd
-cpsed samples/users ${IMPORTDIR}/samples/fingerd.users
+cpsed samples/fingerd.acl ${IMPORTDIR}/samples/fingerd.acl
+cpsed samples/fingerd.motd ${IMPORTDIR}/samples/fingerd.motd
+cpsed samples/fingerd.users ${IMPORTDIR}/samples/fingerd.users
 
 cd ${IMPORTDIR}
 
 # tell them what to do....
-echo "cd ${IMPORTDIR} ; cvs import -m 'Import of Planix fingerd verion $version' src/libexec/fingerd PLANIX $releasetag"
+echo "cd ${IMPORTDIR} ; cvs import -m 'Import of Planix fingerd version $version' src/libexec/fingerd PLANIX $releasetag"
 
 exit 0
