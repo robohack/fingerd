f25206e763c20302c24dd379ad07e17032339ffd
Greg A. Woods <woods@planix.ca>
2000-12-02 03:52:29 +0000

    - some HAVE_ things aren't in the main acconfig.h - add a silly @BOTTOM@

--- acconfig.h
+++ acconfig.h
@@ -1,4 +1,4 @@
-#ident	"@(#)fingerd:$Name:  $:$Id: acconfig.h,v 1.6 1999/01/17 01:12:05 woods Exp $"
+#ident	"@(#)fingerd:$Name:  $:$Id: acconfig.h,v 1.7 2000/12/02 03:52:29 woods Exp $"
 
 @TOP@
 
@@ -13,3 +13,17 @@
 
 /* path to the system config directory */
 #define PATH_SYSCONFDIR		"/etc"
+
+/* do we have inet_ntoa()? */
+#undef HAVE_INET_NTOA
+
+/* do we have socket()? */
+#undef HAVE_SOCKET
+
+/* do we have syslog()? */
+#undef HAVE_SYSLOG
+
+@BOTTOM@
+/*
+ * End Of File.
+ */
