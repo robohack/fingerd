7ff4f0963a447a323bde97d1f0a3d5294fa9c8ba
Greg A. Woods <woods@planix.ca>
2019-01-26 22:37:32 +0000

    commit last dregs of unreleased autotools changes

--- acconfig.h
+++ acconfig.h
@@ -1,12 +1,6 @@
-#ident	"@(#)fingerd:$Name:  $:$Id: acconfig.h,v 1.7 2000/12/02 03:52:29 woods Exp $"
+#ident	"@(#)fingerd:$Name:  $:$Id: acconfig.h,v 1.8 2019/01/26 22:37:32 woods Exp $"
 
 @TOP@
-
-/* The name of the package. */
-#define PACKAGE "fingerd"
-
-/* The package release identifier. */
-#define VERSION	"Pre-release"
 
 /* path to the finger program */
 #define PATH_FINGER		"/usr/bin/finger"
